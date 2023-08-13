063e74d9c764ac37a044854a4410072e93e6bf3c
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-13 16:17:13 -0500

    added inquirer questions

--- svg-generator/index.js
+++
@@ -1,5 +0,0 @@
-const svg = require('svg-builder')
-            .width(300)
-            .height(200);
-
-export function generateLogo = 
--- svg-generator/utils/inquirer.js
+++ svg-generator/utils/inquirer.js
@@ -1,2 +1,29 @@
 import inquirer from 'inquirer'
 
+export const askUserQuestions = inquirer.prompt([
+    {
+        name: 'userText',
+        message: 'Write up to 3 characters to use for your logo',
+        type: 'text'
+    },
+    
+    {
+        name: 'userShape',
+        message: 'Choose a shape to use',
+        type: 'list'
+    },
+    {
+        name: 'userColor',
+        message: 'Type in a color or HEX color code you want to use',
+        type: 'input'
+    }
+])
+.then((answers) => {
+    if (answers.userText.length>3) {
+        console.log('You can only have three characters')
+        return askUserQuestions
+    }
+})
+
+
+
